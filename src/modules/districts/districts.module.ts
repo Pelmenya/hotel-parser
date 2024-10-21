edf22fa95634a7cd5cad2f40c64644daf49098f4
Dmitry Lyapin <lyapindm@yandex.ru>
2024-10-21 15:25:29 +0300

    Add hotels and districts controllers

--- src/modules/districts/districts.module.ts
+++ src/modules/districts/districts.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DistrictsRepository } from './districts.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Districts } from './districts.entity';
+import { DistrictsController } from './districts.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Districts])],
   providers: [DistrictsRepository],
   exports:[DistrictsRepository],
+  controllers: [DistrictsController],
 })
 export class DistrictsModule {}
--- src/modules/hotels/hotels.module.ts
+++ src/modules/hotels/hotels.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { HotelsRepository } from './hotels.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hotels } from './hotels.entity';
+import { HotelsController } from './hotels.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Hotels])],
   providers: [HotelsRepository],
-  exports: [HotelsRepository]
+  exports: [HotelsRepository],
+  controllers: [HotelsController]
 })
 export class HotelsModule {}
