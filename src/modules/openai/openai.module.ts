2a88e5318aeb399251a767f40b9948cdac27d2fa
Dmitry Lyapin <lyapindm@yandex.ru>
2024-10-31 09:40:43 +0300

    Setup OpenAI service

--- src/modules/openai/openai.module.ts
+++ src/modules/openai/openai.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [OpenaiService]
+  imports: [ConfigModule],
+  providers: [OpenaiService],
+  exports: [OpenaiService],
 })
 export class OpenaiModule {}
--- src/modules/openai/openai.service.ts
+++ src/modules/openai/openai.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import OpenAI from 'openai';
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+    private openAI: OpenAI;
+    constructor(
+        private readonly configService: ConfigService
+    )  {
+        this.openAI = new OpenAI({
+            apiKey: this.configService.get('OPENAI_API_KEY'),
+            baseURL: this.configService.get('OPENAI_BASE_API_URL')
+          });
+    }
+
+    async generateHotelDescription() {
+
+    }
+
+}
